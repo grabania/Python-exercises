6e49b133c122d3b26fdfa7a13e3cc28204cfe432
grabania <grabania.work@gmail.com>
2020-03-19 14:00:34 +0000

    Add function too_long. Length concept

--- strings-and-lists.py
+++ strings-and-lists.py
@@ -11,3 +11,15 @@ print("""
 "Is it very long?" Alice asked, for she had heard a good deal of poetry that day."
 "It's long," said the Knight,"but it\'s very, very beautiful."
 """)
+
+
+def too_long(s):
+    length = len(s)
+    if length > 140:
+        return True
+    else:
+        return False
+
+
+print(too_long("I'm a short string!"))
+print(too_long("For score and seven years ago our fathers bought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.!"))
