d71bd9c1e8ed4ad0585a820d7ba88cebc029fbb7
grabania <grabania.work@gmail.com>
2020-03-19 13:34:36 +0000

    Add more complex strings

--- strings-and-lists.py
+++ strings-and-lists.py
@@ -1,1 +1,13 @@
 print("Hello world!")
+
+print("That's Python")
+
+print('I said, "It\'s cool')
+
+
+print('"Is it very long?" Alice asked, for she had heard a good deal of poetry that day.\n"It\'s long," said the Knight,"but it\'s very, very beautiful."')
+
+print("""
+"Is it very long?" Alice asked, for she had heard a good deal of poetry that day."
+"It's long," said the Knight,"but it\'s very, very beautiful."
+""")
