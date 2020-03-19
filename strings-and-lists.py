9ad8269761248fb763bcd05df8a375a06d4b9e8c
grabania <grabania.work@gmail.com>
2020-03-19 19:58:14 +0000

    Add f-strings

--- strings-and-lists.py
+++ strings-and-lists.py
@@ -100,3 +100,8 @@ print(adverbly("quick"))
 
 name = input("hi, what's your name?")
 print("hi " + name + ", nice to meet you!")
+
+name = input("What's your name?\n\n")
+print(f"\nHi, {name}! It's very nice to meet you.\n")
+color = input("What's your favorite color?\n\n")
+print(f"\nAh, {color}, what a lovely color!")
